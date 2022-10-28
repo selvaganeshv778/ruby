6bf7a668e026c17a3bc0051efce49974dce631b9
selvaganeshv778 <116898028+selvaganeshv778@users.noreply.github.com>
2022-10-29 00:01:39 +0530

    comitted from main

---
+++ Default_or_optional.rb
@@ -0,0 +1,9 @@
+def make_internaltional_call(number, international_code=91, area_code=44)
+    puts "dail the number #{number}-#{international_code}-#{area_code}"    
+end
+
+
+make_internaltional_call(78714578880,5,1)
+make_internaltional_call(7871457880)
+
+
