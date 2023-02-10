92c7eab4e884695e149fe53d7b4af2c6e0ab7209
selvaganeshv778 <116898028+selvaganeshv778@users.noreply.github.com>
2023-02-10 21:34:50 +0530

    commiting

--- first.rb
+++ first.rb
@@ -1,1 +1,12 @@
 puts "hello world"
+a = "conversation has be converted into support ticket 8790"
+
+b = a.include?("conversation")
+
+puts b
+
+n = [0,1,2,3,4,5,6,7,8,9,0,10,11,12,13,14,15]
+
+p n[2,7]
+
+
